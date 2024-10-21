880fbd083f3e4cf264555b07062a55d963b6a5b7
Gino Di Settimio <disettimiogino@gmail.com>
2024-10-21 00:35:21 -0300

    Implementación de status code

--- server/api/v1/hardware_routes.py
+++ server/api/v1/hardware_routes.py
@@ -3,11 +3,64 @@ from fastapi import APIRouter
 router = APIRouter(prefix='/hardware')
 
 
-@router.get('/')
-async def get_all():
+@router.get(
+    path='',
+    status_code=200,
+    responses={
+        200: {'description': 'Lista de productos'},
+    },
+    description='Retorna una lista con todos los productos con sus respectivos datos.'
+)
+async def get_all() -> list:
     return []
 
 
-@router.post('/')
-async def create():
+@router.get(
+    path='/{id}',
+    status_code=200,
+    responses={
+        200: {'description': 'Producto encontrado'},
+        422: {'description': 'Id tiene que ser un entero'},
+    },
+    description='Retorna producto con sus respectivos datos via id.'
+)
+async def get_by_id(id: int) -> dict:
+    return {'id': id}
+
+
+@router.post(
+    path='',
+    status_code=201,
+    responses={
+        201: {'description': 'Producto creado'}
+    },
+    description='Crea producto agregandole sus datos e id.'
+)
+async def create() -> dict:
     return {}
+
+
+@router.patch(
+    path='/{id}',
+    status_code=200,
+    responses={
+        200: {'description': 'Producto actualizado.'},
+        422: {'description': 'Id tiene que ser un entero'},
+    },
+    description='Actualiza datos del producto via id.'
+)
+async def update(id: int) -> dict:
+    return {'id': id}
+
+
+@router.delete(
+    path='/{id}',
+    status_code=200,
+    responses={
+        200: {'description': 'Producto eliminado.'},
+        422: {'description': 'Id tiene que ser un entero'},
+    },
+    description='Elimina producto via id.'
+)
+async def delete(id: int) -> None:
+    return None
